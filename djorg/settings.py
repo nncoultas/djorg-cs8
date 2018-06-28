65f70f7a94c96b24aea7d79b55357f66f107cbc0
Nick Coultas <nncoultas@yahoo.com>
2018-06-28 10:00:20 -0700

    Notes caught up to lecture

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note
+from .models import PersonalNote
 
 # Register your models here.
+admin.site.register(Note)
+admin.site.register(PersonalNote)
